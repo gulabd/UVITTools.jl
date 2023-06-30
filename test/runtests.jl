0b14ebac8b41c903fa12e4fd03898e0f985a61f5
Gulab Dewangan <gulabd@iucaa.in>
2023-06-30 17:50:48 +0530

    test update

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,6 @@
 using UVITTools
 using Test
 
-
-@testset "Effective Area calculation" begin
-    @test fuv_grating2_ea(1400.0,order=-2) == 2.7117826719319313
-    @test fuv_grating1_ea(1500.0,order=-2) == 4.23520016495318
-end
-
 @testset "Wavelength calculations" begin
 	@test fuv_grating2_pixel2lamA(-500,order=-2) == 1447.2985865170435
 	@test fuv_grating1_pixel2lamA(-500,order=-2) == 1440.3065041673246
